1f79884e03d43ac601fa07b92172a7edf04aa58d
RuanSilva2167 <ruansilva6721@gmail.com>
2023-02-05 17:41:19 -0300

    adicionando pacotes ao projeto

--- buscarCursos.php
+++ buscarCursos.php
@@ -1,5 +1,21 @@
 <?php
+require 'vendor\autoload.php';
+
 use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
 
 $client = new Client();
-$client->request(method: 'GET', uri: 'https://alura.com.br/cursos-online-programacao/php');
+$resposta = $client->request('GET','https://www.alura.com.br/busca?query=php', [
+    'verify' => false
+]);
+
+$html = $resposta->getBody();
+
+$crawler = new Crawler();
+$crawler->addHtmlContent($html);
+
+$cursos = $crawler->filter('h4.busca-resultado-nome');
+
+foreach ($cursos as $domElement) {
+   echo 'I curso de! '. $domElement->textContent . PHP_EOL;
+}
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,25 +33,18 @@ class ComposerAutoloaderInitc6d3b53d4178263933641eb354a01a54
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInitc6d3b53d4178263933641eb354a01a54::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequirec6d3b53d4178263933641eb354a01a54($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitc6d3b53d4178263933641eb354a01a54::$files;
+        $requireFile = static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        };
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            ($requireFile)($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequirec6d3b53d4178263933641eb354a01a54($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
